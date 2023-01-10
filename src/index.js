be3b2451ca1d49f3e42ae84d2066603e5ce6582f
aatashj1 <frnkxoxo69@gmail.com>
2023-01-10 19:53:56 +0300

    test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+// module.exports = function reverse (n) {
+//     let posNum = Math.abs(n);
+//  let numToString = posNum.toString();
+//  let reverseStr = numToString.split("").reverse().join("");
+// return +reverseStr;
+// }
+
 module.exports = function reverse (n) {
-  
+    let posNum = Math.abs(n);
+    let numToString = posNum.toString();
+    let result = "";
+    for (let i = numToString.length-1; i >= 0; i-- ){
+        result += numToString[i];
+    }  return result;
 }
+
